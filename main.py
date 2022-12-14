1bb9731c52fb782f9de257ab46873534afc5b625
Alex Grivell <alex@Alexs-MacBook-Air.local>
2022-12-15 08:26:53 +1030

    Initial project

---
+++ main.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import json
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v'
+        , 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+               'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_entry.insert(0, password)
+
+
+def save():
+    website = web_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Error", message="A field has been left empty")
+    else:
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
+        finally:
+            web_entry.delete(0, END)
+            email_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
+def find_password():
+    website = web_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exist")
+
+
+window = Tk()
+window.title("Lockr")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(height=200, width=200)
+logo = PhotoImage(file="res/logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.grid(row=0, column=1)
+
+web_label = Label(text="Website:")
+web_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+web_entry = Entry(width=21)
+web_entry.focus()
+web_entry.grid(row=1, column=1)
+
+search_button = Button(text="Search", width=10, command=find_password)
+search_button.grid(row=1, column=2)
+
+email_entry = Entry(width=37)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+password_entry = Entry(width=22)
+password_entry.grid(row=3, column=1)
+
+gen_password_button = Button(text="Generate", width=10, command=generate_password)
+gen_password_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
